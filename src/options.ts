c453779ca21b0b90445a24c84bddc15f00c9661d
ahas <dev@ahas.io>
2022-06-27 11:08:27 +0900

    Add features Expose, Exclude and IsEmpty

--- src/options.ts
+++ src/options.ts
@@ -1,11 +1,14 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { TypeHelpOptions } from "class-transformer";
-import { IsOptional, ValidateNested, ValidationOptions } from "class-validator";
+import { Exclude, ExcludeOptions, Expose, ExposeOptions, TypeHelpOptions } from "class-transformer";
+import { IsEmpty, IsOptional, ValidateNested, ValidationOptions } from "class-validator";
 
 export interface ConverterOptions extends ValidationOptions {
     optional?: boolean;
     api?: boolean;
     nested?: boolean;
+    empty?: boolean;
+    exclude?: ExcludeOptions | boolean;
+    expose?: ExposeOptions | boolean;
 }
 
 export function applyOptions(
@@ -24,5 +27,17 @@ export function applyOptions(
 
     if (options.api) {
         ApiProperty({ required: !options.optional, type: typeFunction?.() })(target, propertyKey);
+    }
+
+    if (options.exclude) {
+        Exclude(typeof options.exclude === "object" ? options.exclude : undefined)(target, propertyKey);
+    }
+
+    if (options.expose) {
+        Expose(typeof options.expose === "object" ? options.expose : undefined)(target, propertyKey);
+    }
+
+    if (options.empty) {
+        IsEmpty(options)(target, propertyKey);
     }
 }
